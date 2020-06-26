dd2520ebeca3ae9f02da7cf0470a3ddd09e34396
Payoshika <59700005+Payoshika@users.noreply.github.com>
2020-06-26 09:50:34 +0900

    changed landing page

--- src/MobileLandingPage.jsx
+++ src/MobileLandingPage.jsx
@@ -8,15 +8,21 @@ class MobilelandingPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <Link to={`/mobileonetoonepage/`} className="model-display d-flex flex-column justify-content-center align-items-center">
-          <h4>One to One Conversion</h4>
-          <img src={"./img/onetooneimg.png"} className="img-fluid border rounded" alt=""/>
-        </Link>
-        <Link to={`/mobilelistconversionpage/`} className="model-display d-flex flex-column justify-content-center align-items-center">
-          <h4>List mode</h4>
-          <img src={"./img/listimg.png"} className="img-fluid border rounded" alt=""/>
-        </Link>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <div>
+              <Link to={`/mobileonetoonepage/`} className="model-display d-flex flex-column justify-content-center align-items-center">
+                <h4>One to One Conversion</h4>
+                <img src={"./img/onetooneimg.png"} className="img-fluid border rounded" alt=""/>
+              </Link>
+              <Link to={`/mobilelistconversionpage/`} className="model-display d-flex flex-column justify-content-center align-items-center">
+                <h4>List mode</h4>
+                <img src={"./img/listimg.png"} className="img-fluid border rounded" alt=""/>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
      )
   }
