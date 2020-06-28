29d39c1a182d2797450b3e4849e8992a1c2b395a
Payoshika <59700005+Payoshika@users.noreply.github.com>
2020-06-28 20:28:41 +0900

    modified the css of the landing page

--- src/MobileLandingPage.jsx
+++ src/MobileLandingPage.jsx
@@ -12,11 +12,15 @@ class MobilelandingPage extends React.Component {
         <div className="row">
           <Link to={`/mobileonetoonepage/`} className="model-display col-12 d-flex flex-column justify-content-center align-items-center mb-2">
             <h4>One to One Conversion</h4>
-            <img src={"./img/onetooneimg.png"} className="img-fluid border rounded" alt=""/>
+            <div className="img-container">
+              <img src={"./img/onetooneimg.png"} className="img-fluid border rounded" alt=""/>
+            </div>
           </Link>
           <Link to={`/mobilelistconversionpage/`} className="model-display col-12 d-flex flex-column justify-content-center align-items-center mb-2">
             <h4>List mode</h4>
-            <img src={"./img/listimg.png"} className="img-fluid border rounded" alt=""/>
+            <div className="img-container">
+              <img src={"./img/listimg.png"} className="img-fluid border rounded" alt=""/>
+            </div>
           </Link>
         </div>
       </div>
